dc8f7721ac0ecad62d40c77dd95a3c7c7071311d
KingDhruva07 <114047896+KingDhruva07@users.noreply.github.com>
2022-09-28 11:42:02 +0530

    Update Login.jsx

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
   	<form action="/admin" >
   <div className="mb-3 mx-5">
     <label htmlFor="exampleInputEmail1" className="form-label">
-      Email address
+      User Name
     </label>
     <input
       type="email"
@@ -17,9 +17,7 @@ function Login() {
       id="exampleInputEmail1"
       aria-describedby="emailHelp"
     />
-    <div id="emailHelp" className="form-text">
-      We'll never share your email with anyone else.
-    </div>
+    
   </div>
   <div className="mb-3 mx-5">
     <label htmlFor="exampleInputPassword1" className="form-label">
@@ -31,14 +29,9 @@ function Login() {
       id="exampleInputPassword1"
     />
   </div>
-  <div className="mb-3 form-check mx-5">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-    <label className="form-check-label" htmlFor="exampleCheck1">
-      Check me out
-    </label>
-  </div>
+  
   <button type="submit" className="btn btn-primary mx-5">
-    Submit
+    Log in
   </button>
 </form>
     
