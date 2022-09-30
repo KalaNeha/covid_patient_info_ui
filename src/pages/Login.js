dae4cd5630824b49bf0ac57778b44b1363854119
pragyagh <pragya_ghiya@optum.com>
2022-09-30 20:44:29 +0530

    first commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,50 +1,80 @@
-import React from 'react';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Home from "./Home";
 
+import "./styles.css";
 
+function App() {
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-function Login() {
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    const userData = database.find((user) => user.username === uname.value);
+
+    if (userData) {
+      if (userData.password !== pass.value) {
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+
   return (
- <>
-  <div className="text-center"> Log in </div>
-  	<form action="/admin" >
-  <div className="mb-3 mx-5">
-    <label htmlFor="exampleInputEmail1" className="form-label">
-      User Name
-    </label>
-    <input
-      type="email"
-      className="form-control"
-      id="exampleInputEmail1"
-      aria-describedby="emailHelp"
-    />
-<<<<<<< HEAD:src/pages/Login.js
-=======
-    
->>>>>>> dc8f7721ac0ecad62d40c77dd95a3c7c7071311d:src/component/Login.jsx
-  </div>
-  <div className="mb-3 mx-5">
-    <label htmlFor="exampleInputPassword1" className="form-label">
-      Password
-    </label>
-    <input
-      type="password"
-      className="form-control"
-      id="exampleInputPassword1"
-    />
-  </div>
-<<<<<<< HEAD:src/pages/Login.js
-  <button type="submit" className="btn btn-primary mx-5">
-    Login
-=======
-  
-  <button type="submit" className="btn btn-primary mx-5">
-    Log in
->>>>>>> dc8f7721ac0ecad62d40c77dd95a3c7c7071311d:src/component/Login.jsx
-  </button>
-</form>
-    
-</>
+    <div className="app">
+      <div className="login-form">
+        <div className="title">Sign In</div>
+        {isSubmitted ? <Home/> : renderForm}
+      </div>
+    </div>
   );
 }
 
-export default Login;
+export default App;
