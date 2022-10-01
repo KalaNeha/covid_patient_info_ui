bac13abc8a6fc0c93d7904f5cb5e97ef552e3f39
KingDhruva07 <114047896+KingDhruva07@users.noreply.github.com>
2022-10-01 10:54:03 +0530

    Update Nav.js

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -7,7 +7,7 @@ function Nav(){
         <>
             <div><nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Covid  </a>
+    <a className="navbar-brand" href="/">CPIS  </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
